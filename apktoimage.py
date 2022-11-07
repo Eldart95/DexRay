f098b609603e0b890bd74e76d39f3c8bac44fac2
eldar <eldartakach@gmail.com>
2022-11-08 00:32:55 +0200

    Add multithreading functionality.

--- apktoimage.py
+++ apktoimage.py
@@ -1,8 +1,10 @@
 import sys
-import math
+import threading
 from androguard.core.bytecodes.apk import APK
 from PIL import Image
 
+def log(func_name:str, to_log:str):
+    print(f"Function: {func_name} is Logging: {to_log}")
 
 def get_dex_bytes(apk: APK) -> bytes:
     for f in apk.get_files():
@@ -20,7 +22,9 @@ def generate_png(apk: APK, filename: str, folder: str):
 
 
 def generate_png_of_text(filename: str, folder: str):
+    print(f"Thread {threading.get_native_id} is in generate_png_of_text function.")
     stream = bytes()
+    amount_of_images_encoded = 0
     with open(filename) as file:
         lines = file.readlines()
         for line in lines:
@@ -31,16 +35,25 @@ def generate_png_of_text(filename: str, folder: str):
             current_len = len(stream)
             image = Image.frombytes(mode='L', size=(1, current_len), data=stream)
             image.save(f"{folder}/{line}.png")
+            amount_of_images_encoded += 1
+    print(f"Successfully encoded {amount_of_images_encoded} images.")
 
 
 if __name__ == "__main__":
 
     if len(sys.argv) == 1:
-        generate_png_of_text("goodware_hashes.txt", "images/goodware")
-        generate_png_of_text("malware_hashes.txt", "images/malware")
-        print("Think im done")
+        print("No arguments provided, therefor will decode goodware and malware hashes texts to png file.")
+        goodware_thread = threading.Thread(target=generate_png_of_text, args=("goodware_hashes.txt", "images/goodware"))
+        malware_thread = threading.Thread(target=generate_png_of_text, args=("malware_hashes.txt", "images/malware"))
 
-    """
+        goodware_thread.start()
+        malware_thread.start()
+
+        goodware_thread.join()
+        malware_thread.join()
+        print("Done encoding... exiting.")
+        sys.exit(0)
+
     elif len(sys.argv) < 3:
         raise Exception("[!] Usage: python3 apktoimage.py APK DESTINATION")
 
@@ -53,6 +66,6 @@ if __name__ == "__main__":
 
         print(f"Image successfully generated from {filename}")
     except Exception as e:
-        print("[!] An exception occured with: {}".format(filename))
+        print("[!] An exception occurred with: {}".format(filename))
         print("Exception: {}".format(e))
-    """
+
